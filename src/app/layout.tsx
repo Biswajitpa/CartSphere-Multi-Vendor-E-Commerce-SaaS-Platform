25a6f147664d28e8a4a44e855e84193281d2dec2
Biswajit <pattanaikbiswajit07@gmail.com>
2026-06-26 13:54:20 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-
 import "./globals.css";
-
 import Provider from "@/Provider";
 import StoreProvider from "@/redux/StoreProvider";
 import InitUser from "@/InitUser";
-
-
-
-
+import AppShell from "@/components/AppShell";
 
 export const metadata: Metadata = {
   title: "Multi-Cart",
@@ -22,15 +17,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="w-full min-h-screen bg-linear-to-b from-black"> 
-       
-       <Provider>
-        <StoreProvider>
-          <InitUser/>
-        {children}
-        </StoreProvider>
+      <body className="w-full min-h-screen bg-linear-to-b from-black">
+        <Provider>
+          <StoreProvider>
+            <AppShell>
+              <InitUser />
+              {children}
+            </AppShell>
+          </StoreProvider>
         </Provider>
-       
       </body>
     </html>
   );
